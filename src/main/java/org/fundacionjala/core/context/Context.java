7b27d7042a8978571fa33a928cf244f94376d9c7
carlosmc23 <kenshinmc23@gmail.com>
2020-06-10 17:29:09 -0400

    implement project api base

---
+++ src/main/java/org/fundacionjala/core/context/Context.java
@@ -0,0 +1,74 @@
+package org.fundacionjala.core.context;
+
+import io.restassured.response.Response;
+import org.fundacionjala.core.entities.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Context {
+
+    private User user;
+    private Map<String, Response> responses;
+    private Map<String, List<String>> mapIds;
+
+    /**
+     * Initializes an instance of Context class.
+     */
+    public Context() {
+        initializeValues();
+    }
+
+    /**
+     * Initializes context values.
+     */
+    public void initializeValues() {
+        responses = new HashMap<>();
+        mapIds = new HashMap<>();
+        user = null;
+    }
+
+    /**
+     * Saves response according to key.
+     *
+     * @param keyword  key identifier.
+     * @param response response object.
+     */
+    public void saveResponse(final String keyword, final Response response) {
+        responses.put(keyword, response);
+    }
+
+    /**
+     * Gets context responses.
+     *
+     * @return context responses.
+     */
+    public Map<String, Response> getResponses() {
+        return responses;
+    }
+
+    /**
+     * Saves id in a map.
+     *
+     * @param keyword map key.
+     * @param id      project id.
+     */
+    public void saveIds(final String keyword, final String id) {
+        if (!mapIds.containsKey(keyword)) {
+            mapIds.put(keyword, new ArrayList<>());
+        }
+        mapIds.get(keyword).add(id);
+    }
+
+    /**
+     * Gets saved map by key.
+     *
+     * @param keyword map key.
+     * @return id values.
+     */
+    public List<String> getIdsByKey(final String keyword) {
+        return mapIds.getOrDefault(keyword, new ArrayList<>());
+    }
+}
