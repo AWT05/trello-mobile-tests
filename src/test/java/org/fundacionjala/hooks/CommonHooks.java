3579998184cfc09ae68b41587af02d53cd0d1ff1
Lucero Quiroga Perez <35902165+luqp@users.noreply.github.com>
2020-06-11 09:12:28 -0400

    [develop] Lucero - Requests api, stepdefs and hooks to clean data (#11)
    * Adds requests by api.
    
    * Clean code.
    
    * Delete space.

---
+++ src/test/java/org/fundacionjala/hooks/CommonHooks.java
@@ -0,0 +1,30 @@
+package org.fundacionjala.hooks;
+
+import io.cucumber.java.After;
+import org.fundacionjala.core.api.RequestManager;
+import org.fundacionjala.core.context.Context;
+import org.fundacionjala.utils.EndPointsEnum;
+
+public final class CommonHooks {
+
+    private static final int CLEAN_CONTEXT_ORDER = 20;
+    private final Context context;
+    private final RequestManager requestManager;
+
+    public CommonHooks(final Context context, final RequestManager requestManager) {
+        this.context = context;
+        this.requestManager = requestManager;
+    }
+
+    /**
+     * Deletes Items if they were created.
+     */
+    @After(value = "@cleanDataApi", order = CLEAN_CONTEXT_ORDER)
+    public void cleanTestsData() {
+        for (EndPointsEnum item : EndPointsEnum.values()) {
+            context.getIdsByKey(item.toString()).forEach(id -> requestManager
+                    .init(context)
+                    .delete(item.getEndPoint().concat(id)));
+        }
+    }
+}
